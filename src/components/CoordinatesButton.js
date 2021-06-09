a326011c377899643fd9e9f6a081750f72cfa4f5
Marc Steele <marcsteele2014@gmail.com>
2021-06-09 20:26:45 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -6,12 +6,12 @@ export default class CoordinatesButton extends Component {
         super ()
 
     }
-  
+
         handleClick = (event) => {
             event.persist()
             setTimeout(() => this.props.onDelayedClick(event), this.props.delay)
         }
-    
+
         render() {
             return <button onClick={this.handleClick}></button>
         }
